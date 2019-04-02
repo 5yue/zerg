4cd70895968b77da80190d7922c1faac114a0643
XiaoYaoJingLxl <1931078423@qq.com>
2019-04-02 20:07:59 +0800

    第一次优化获取参数，比以前更加符合面向对象的编程思想

--- application/api/validate/BaseValidate.php
+++ application/api/validate/BaseValidate.php
@@ -24,8 +24,14 @@ class BaseValidate extends Validate
 
         $result = $this->check($params);
         if(!$result){
-            $e = new ParameterException('');
-            $e->msg = $this->error;
+            $e = new ParameterException([//初始化成员变量，赋值
+                //也可以可选参数赋值，即，部分参数的赋值
+                'msg' => $this->error,
+//                'code'=> 400,
+//                'errorCode'=>10002
+            ]);//访问外部成员变量赋值时是没有[]的内容的。
+            //[]内的方法赋值时，更加符合面向对象的特性
+//            $e->msg = $this->error;//访问外部成员变量
             throw $e;
 //            $error = $this->error;
 //            throw new Exception($error);
--- application/lib/exception/BaseException.php
+++ application/lib/exception/BaseException.php
@@ -10,6 +10,7 @@ namespace app\lib\exception;
 
 
 use think\Exception;
+use Throwable;
 
 class BaseException extends Exception
 {
@@ -19,4 +20,22 @@ class BaseException extends Exception
     public $msg = 'Parameter ERROR';
     //自定义的错误代码
     public $errorCode = 10000;
+
+    //构造函数
+    public function __construct($params = [])
+    {
+        if (!is_array($params)){
+            return ;//这里认为如果不是数组,不改变变量的默认值
+//            throw new Exception('参数必须是整数');
+        }
+        if (array_key_exists('code',$params)){//判断数组中是否有code
+            $this->code = $params['code'];
+        }
+        if (array_key_exists('msg',$params)){//判断数组中是否有code
+            $this->msg = $params['msg'];
+        }
+        if (array_key_exists('errorCode',$params)){//判断数组中是否有code
+            $this->errorCode = $params['errorCode'];
+        }
+    }
 }
